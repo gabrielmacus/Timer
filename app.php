350f23a0d11b1f082a483bd9705073ad48c15e38
gabrielmacus <gabrielmacus@gmail.com>
2017-10-04 13:29:34 -0300

    sse

--- app.php
+++ app.php
@@ -31,7 +31,7 @@ try
 //include ("app/auth/check-login.php");
 ?>
 <script>
-    wsUrl='ws://<?php echo getHostByName(getHostName()); ?>:8080';
+    wsUrl='ws://192.168.1.230:8080';
     var app = angular.module('app', ['ngRoute','ngWebSocket']);
 </script>
 
